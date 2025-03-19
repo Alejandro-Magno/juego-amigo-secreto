187416120d5226f305c102758a7adec7a66a34b3
Alejandro-Magno <144861436+Alejandro-Magno@users.noreply.github.com>
2025-03-18 21:00:52 -0500

    Se crea el archivo .js y la primera función
    Se crea el archivo JavaScript que dará las funcionalidades al juego de amigo secreto, y a su vez se implementa la funcionalidad de agregar o adicionar amigos a la lista

---
+++ app.js
@@ -0,0 +1,21 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades 
+// en lógica de programación. Aquí deberás desarrollar 
+// la lógica para resolver el problema.
+// app.js
+
+let amigos = [];
+const listaAmigos = document.getElementById('listaAmigos');
+const amigoInput = document.getElementById('amigo');
+const resultado = document.getElementById('resultado');
+
+function agregarAmigo() {
+    const nombreAmigo = amigoInput.value.trim();
+    if (nombreAmigo) {
+        amigos.push(nombreAmigo);
+        actualizarListaAmigos();
+        amigoInput.value = ''; // Limpia el input
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
+
